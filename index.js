f8960c25085368d8915b17b481f536f7dad6df4c
yuronghui <yuronghui@singworld.net>
2018-11-26 17:49:15 +0800

    no message

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ const Queue = require('./src/queue');
 
 module.exports = (app = {}, config = {}) => {
   let redisPlugin = app.Plugin('redis'),
-    redisDb = null;
+    redisDb = null,
+    _queues = new Map();
   if(redisPlugin && redisPlugin.Redis && redisPlugin.Redis.db){
     redisDb = redisPlugin.Redis.db;
   }
-  app.Queue = new Queue({redis: redisDb, ctx: app });
+  app.Queue = function(queueName, options = {}) {
+    if(!_queues.has(queueName)){
+      _queues.set(queueName, new Queue({redis: redisDb, ctx: app, options }));
+    }
+    return _queues.get(queueName);
+  }
   if(app.addAppProp) app.addAppProp('Queue', app.Queue);
   return app;
 }
--- src/queue.js
+++ src/queue.js
@@ -7,27 +7,32 @@ class Queue {
   constructor(opts = {}) {
     this.redis = opts.redis;
     this.ctx = opts.ctx;
+    this.options = {
+      maxlen: 0, //最大长度
+      ...(opts.options || {})
+    };
     if(!this.redis) console.warn('Queue Redis 没找到实例');
   }
 
-  push(listkey, value) {
-    return new Promise((resolve, reject) => {
-      if(!this.redis) throw this.ctx.error('入队失败');
-      this.redis.lpush(listkey, value, (err, reply) => {
-        if (err) reject(err);
-        resolve(value);
-      });
-    });
+  async push(listkey, value) {
+    let countResult = await this.ctx.catchErr(this.redis.listCount(listkey));
+    if(countResult.err) throw this.ctx.error(countResult.err);
+    if(this.options.maxlen && this.options.maxlen >= countResult.data){
+      return null;
+    }else{
+      let result = await this.ctx.catchErr(this.redis.listPush(listkey, value));
+      if(result.err) throw this.ctx.error(result.err);
+      return result.data;
+    }
   }
 
-  pop(listkey, times = 0) {
-    return new Promise((resolve, reject) => {
-      if(!this.redis) throw this.ctx.error('出队失败');
-      this.redis.brpop(listkey, times, (err, repl) => {
-        if (err) reject(err);
-        resolve(repl[1]);
-      });
-    });
+  async pop(listkey, times = 0) {
+    let countResult = await this.ctx.catchErr(this.redis.listCount(listkey));
+    if(countResult.err) throw this.ctx.error(countResult.err);
+    if(!countResult.data) return null;
+    let result = await this.ctx.catchErr(this.redis.brpop(listkey, times));
+    if(result.err) throw this.ctx.error(result.err);
+    return result.data;
   }
 }
 module.exports = Queue;
